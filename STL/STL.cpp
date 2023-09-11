b03739c80f67d61ffa6ac2970a6f2eceeffeb9a0
tekater <120014627+tekater@users.noreply.github.com>
2023-09-11 23:32:32 +0900

    Update STL.cpp

--- STL/STL.cpp
+++ STL/STL.cpp
@@ -11,7 +11,6 @@ using std::cout;
 
 //#define STL_ARRAY
 #define STL_VECTOR
-#define STL_
 
 template<typename T>
 void vector_properties(const std::vector<T>& vec) {
@@ -41,8 +40,20 @@ int main()
 
 #ifdef STL_VECTOR
     //vector - это контейнер, который хранит данные в виде динамического массива.
-
+    
     std::vector<int> vec = { 3,5,8,13,21,1,2,3,4,5 };
+
+    try {
+        cout << "TRY\n";
+        for (int i = 0; i < vec.size() * 2; i++) {
+            cout << vec.at(i) << tab;
+        }
+    }
+    catch (const std::exception& e) {
+        cout << endl;
+        std::cerr << e.what() << endl;
+        cout << endl << endl;
+    }
 
     for (int i = 0; i < vec.size(); i++) {
         cout << vec[i] << tab;
@@ -67,8 +78,6 @@ int main()
     vec.push_back(55);
     vector_properties(vec);
 
-    
-
     cout << "Vec2\n";
     std::vector<int> vec2(100);
     vector_properties(vec2);
@@ -79,15 +88,52 @@ int main()
     cout << "<Double>\n";
     vector_properties(d_vec);
 
+    //cout << d_vec.front() << endl;
+    //cout << d_vec.back() << endl;*/
 
+    
+    cout << "Insert\n";
+    int index;
+    int value;
 
+    index = 2; //cout << "Введите индекс добавляемого значения: "; cin >> index;
+    value = 50; //cout << "Введите значение добавляемого элемента: "; cin >> value;
+
+    vec.resize(5);
+    for (int i : vec) {
+        cout << i << tab;
+    }
+    cout << endl;
+    vec.insert(vec.begin() + index, value);
+
+    for (int i = 0; i < vec.size(); i++) {
+        cout << vec.at(i) << tab;
+    }
+    cout << endl;
+    vector_properties(vec);
+
+    for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it) {
+        cout << *it << tab;
+    }
+    cout << endl;
+
+    for (std::vector<int>::reverse_iterator it = vec.rbegin(); it != vec.rend(); ++it) {
+        cout << *it << tab;
+    }
+    cout << endl;
+
+    for (int i : vec) {
+        cout << i << tab;
+    }
+    cout << endl;
+
+    vec.erase(vec.begin() + index);
+
+    for (int i : vec) {
+        cout << i << tab;
+    }
+    cout << endl;
 #endif //STL_VECTOR
-
-#ifdef STL_
-    //
-
-
-#endif // STL_
 
 
 
